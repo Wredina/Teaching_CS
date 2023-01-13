828d57badecd8bb373b58ef35c82764cd18956eb
Sofiya <n.d.dead@mail.ru>
2023-01-13 15:03:00 +0300

    added task 05/07 and add homework

---
+++ task02_homework/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+// a = 5; b = 7->max = 7
+// a = 2 b = 10->max = 10
+// a = -9 b = -3->max = -3
+
+Console.WriteLine("введите первое число");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numOne > numTwo)
+{
+  Console.WriteLine($"Максимальное число = {numOne}, минимальное число = {numTwo}");
+}
+else if (numTwo > numOne)
+{
+  Console.WriteLine($"Максимальное число = {numTwo} минимальное число = {numOne}");
+}
+else
+{
+  Console.WriteLine($"Чсло {numOne} равно числу {numTwo}");
+}
---
+++ task04_homework/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+  Console.Write($"Максимальное число из {a}, {b}, {c} = {a}");
+}
+else if (b > a && b > c)
+{
+  Console.Write($"Максимальное число из {a}, {b}, {c} = {b}");
+}
+else
+{
+  Console.Write($"Максимальное число из {a}, {b}, {c} = {c}");
+}

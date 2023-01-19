fb657a46b22b072b765ae9c783d565e01ea3e0fa
Sofiya <n.d.dead@mail.ru>
2023-01-19 13:35:30 +0300

    added task 10,13,15 homework number 2

---
+++ task13_homework_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int NumTree(int num)
+{
+  while (num > 1000)
+  {
+    num = num / 10;
+  }
+  return num % 10;
+}
+
+Console.WriteLine("Введите случайное число");
+int number = Convert.ToInt32(Console.ReadLine());
+// int number = new Random().Next(1, 10000000);
+// Console.WriteLine($"Случайное число {number}");
+
+if (number < 100)
+{
+  Console.WriteLine("третьего числа нет");
+}
+else
+{
+  int res = NumTree(number);
+  Console.WriteLine($"Третье число {res}");
+}
---
+++ task15_homework_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool DayOfWeek(int num)
+{
+  return num == 6 || num == 7;
+}
+
+Console.WriteLine("Введите число дня недели от 1 до 7-ми");
+int number = Convert.ToInt32(Console.ReadLine());
+// int number = new Random().Next(1, 8);
+// Console.WriteLine($"Случайное число {number}");
+
+bool res = DayOfWeek(number);
+string day = res ? "выходной день" : "будний день";
+
+Console.WriteLine(day);

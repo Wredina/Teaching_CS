6ab06fc10ea8d34da9de03bdea88d9bbddc4c9a1
Sofiya <n.d.dead@mail.ru>
2023-01-20 20:13:31 +0300

    added task 17,18,20,22

---
+++ task18/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+string Quarter(string num)
+{
+
+  // if (num == "1") return "x > 0 и y > 0";
+  // if (num == "2") return "x < 0 и y > 0";
+  // if (num == "3") return "x < 0 и y < 0";
+  // if (num == "4") return "x > 0 и y < 0";
+  // return "Введены некорректные координаты";
+
+  switch (num)
+  {
+    case "1": return "Допустимые координаты: x > 0 и y > 0";
+    case "2": return "Допустимые координаты: x < 0 и y > 0";
+    case "3": return "Допустимые координаты: x < 0 и y < 0";
+    case "4": return "Допустимые координаты: x > 0 и y < 0";
+    default: return "Неккоректный ввод!";
+  }
+}
+
+Console.Write("Введите номер плоскости: ");
+string number = Console.ReadLine();
+
+string quarter = Quarter(number);
+
+Console.WriteLine(quarter);
+

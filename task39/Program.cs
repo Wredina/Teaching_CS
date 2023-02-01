4456917be2f33861bb146e06367e56ebd3e004dc
Sofiya <n.d.dead@mail.ru>
2023-02-01 15:28:04 +0300

    add task 39

---
+++ task39/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+  int[] array = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = rnd.Next(min, max + 1);
+  }
+  return array;
+}
+
+void ReverseArray(int[] array)
+{
+  for (int i = 0; i < array.Length / 2; i++)
+  {
+    int temp = array[i];
+    array[i] = array[array.Length - 1 - i];
+    array[array.Length - 1 - i] = temp;
+  }
+}
+
+int[] arr = CreateArrayRndInt(5, 1, 10);
+Console.WriteLine(String.Join(',', arr));
+ReverseArray(arr);
+Console.WriteLine(String.Join(',', arr));
+Array.Reverse(arr);
+Console.WriteLine(String.Join(',', arr));

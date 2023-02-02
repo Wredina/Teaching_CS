3e8fb323cd75c6370e1c9cada4d111b2152e563b
Wredina <79811996+Wredina@users.noreply.github.com>
2023-02-02 22:23:20 +0300

    add task 40,42,44,45

---
+++ task44/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи.Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5-> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] FibonacciNum(int size)
+{
+ int[] fibArr = new int[size];
+ fibArr[0] = 0;
+ fibArr[1] = 1;
+ for (int i = 2; i < fibArr.Length; i++)
+ {
+  fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+ }
+ return fibArr;
+}
+
+int[] fibonacci = FibonacciNum(7);
+Console.WriteLine(String.Join(' ', fibonacci));
---
+++ task45/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 45: Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного
+// копирования.
+
+int[] ArrOne(int size)
+{
+ int[] array = new int[size];
+ for (int i = 0; i < array.Length; i++)
+ {
+  array[i] += new Random().Next(1, 10);
+ }
+ return array;
+}
+
+int[] ArrTwo(int[] array, int size)
+{
+ int[] newArr = new int[size];
+ for (int i = 0; i < array.Length; i++)
+ {
+  newArr[i] += array[i];
+ }
+ return newArr;
+}
+
+Console.Write("введите длину массива: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+int[] arrOne = ArrOne(sizeArr);
+Console.WriteLine(String.Join(", ", arrOne));
+int[] arrTwo = ArrTwo(arrOne, sizeArr);
+Console.WriteLine(String.Join(", ", arrTwo));

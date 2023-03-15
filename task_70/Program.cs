72a9229e3a3dbf7a0e026627740c3e01a88d6898
Wredina <79811996+Wredina@users.noreply.github.com>
2023-03-15 16:55:01 +0300

    added 70,71 task

---
+++ task_70/Program.cs
@@ -0,0 +1,25 @@
+﻿// # По данному целому неотрицательному n вычислите
+// # значение n!. N! = 1 * 2 * 3 * … * N (произведение всех
+// # чисел от 1 до N) 0! = 1 Решить задачу используя цикл
+// # while
+// # Input: 5
+// # Output: 120
+
+System.Console.WriteLine("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 0 || n == 1)
+{
+ System.Console.WriteLine(1);
+}
+else
+{
+ int i = 1;
+ int res = 1;
+ while (i <= n)
+ {
+  res *= i;
+  i++;
+ }
+ System.Console.WriteLine(res);
+}
---
+++ task_71/Program.cs
@@ -0,0 +1,31 @@
+﻿// # Дано натуральное число A > 1. Определите, каким по
+// # счету числом Фибоначчи оно является, то есть
+// # выведите такое число n, что φ(n) = A. Если А не
+// # является числом Фибоначчи, выведите число - 1.
+// # Input: 5
+// # Output: 6
+// # 0, 1, 1, 2, 3, 5, 8
+
+System.Console.WriteLine("ввелдите число больше 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int fib1 = 0;
+int fib2 = 1;
+int fib3 = 0;
+int count = 2;
+while (fib2 < n)
+{
+ fib3 = fib1 + fib2;
+ fib1 = fib2;
+ fib2 = fib3;
+ count++;
+}
+
+if (n == fib2)
+{
+ System.Console.WriteLine(count);
+}
+else
+{
+ System.Console.WriteLine(-1);
+}
